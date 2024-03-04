42e0bb0e039305a78017d8fe4ad57a81f8537fcb
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2024-03-04 21:50:21 +0300

    add input value & render in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
+import { useState } from 'react';
 import styles from './css-modules/app.module.css';
 
 export const App = () => {
+	let [value, setValue] = useState('');
+	const [list, setList] = useState([]);
+	let [error, setError] = useState('');
+
+	function onInputButtonClick() {
+		const promptValue = prompt();
+		value = setValue(promptValue);
+		console.log(promptValue);
+	}
+
 	return (
 		<div className={styles.app}>
 			<h1 className={styles['page-heading']}>Ввод значения</h1>
 			<p className={styles['no-margin-text']}>
 				Текущее значение <code>value</code>: "
-				<output className={styles['current-value']}></output>"
+				<output className={styles['current-value']}>{value}</output>"
 			</p>
 			<div className={styles.error}>
 				Введенное значение должно содержать минимум 3 символа
 			</div>
 			<div className={styles['buttons-container']}>
-				<button className={styles.button}>Ввести новое</button>
+				<button className={styles.button} onClick={onInputButtonClick}>
+					Ввести новое
+				</button>
 				<button className={styles.button} disabled>
 					Добавить в список
 				</button>
